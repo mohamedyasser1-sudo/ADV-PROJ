a1a008a1c68212655896a023d8aceb660927a61c
Mohamed Yasser <mohamedyasser71230@gmail.com>
2020-01-09 23:27:20 +0200

    edit signup

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,71 @@
 
 
 })(jQuery)
+
+
+
+// HOST START
+			$(".host .register").click(function(){
+				console.log("Mohamed Yasser1");
+				
+			 $(".host").css("backgroundColor", "#3a3d6e");
+            $('.host .active').fadeOut(100, function () {
+               
+                $(this).removeClass('active').next().addClass('active').fadeIn();
+				
+
+            });
+			});
+			
+			
+			$(".host .backstep").click(function(){
+				console.log("Mohamed Yasser2");
+				$(".host").css("backgroundColor", "#fff");
+            $('.host .active').fadeOut(100, function () {
+               
+                $(this).removeClass('active').next().addClass('active').fadeIn();
+				
+				if ($(this).is(':last-child')) {
+						$('.host div').eq(0).addClass('active').fadeIn();
+					} 
+				
+
+            });
+			});
+			
+			
+			// HOST END
+			
+			
+			
+			// advertise START
+			$(".advertise .register").click(function(){
+				console.log("Mohamed Yasser1");
+				
+			 $(".advertise").css("backgroundColor", "#3a3d6e");
+            $('.advertise .active').fadeOut(100, function () {
+               
+                $(this).removeClass('active').next().addClass('active').fadeIn();
+				
+
+            });
+			});
+			
+			
+			$(".advertise .backstep").click(function(){
+				console.log("Mohamed Yasser2");
+				$(".advertise").css("backgroundColor", "#fff");
+            $('.advertise .active').fadeOut(100, function () {
+               
+                $(this).removeClass('active').next().addClass('active').fadeIn();
+				
+				if ($(this).is(':last-child')) {
+						$('.advertise div').eq(0).addClass('active').fadeIn();
+					} 
+				
+
+            });
+			});
+			
+			
+			// advertise END

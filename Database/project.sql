2d34d9943b3ce58200f4c5981750a7bc1ddd2d86
Ehab Elshamy <ehab_elshamy82@yahoo.com>
2020-01-31 14:44:26 +0200

    last database

--- Database/project.sql
+++ Database/project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2020 at 01:44 PM
+-- Generation Time: Jan 31, 2020 at 01:38 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -31,15 +31,107 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL
+  `password` varchar(25) NOT NULL,
+  `type` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `admin` (`id`, `username`, `password`, `type`) VALUES
+(1, 'admin', 'admin', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `advertise`
+--
+
+CREATE TABLE `advertise` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `pagetype` varchar(25) NOT NULL,
+  `pagename` varchar(50) NOT NULL,
+  `pageurl` varchar(100) NOT NULL,
+  `pagedescription` varchar(200) NOT NULL,
+  `description1` varchar(100) NOT NULL,
+  `image1` varchar(200) NOT NULL,
+  `description2` varchar(100) NOT NULL,
+  `image2` varchar(200) NOT NULL,
+  `description3` varchar(100) NOT NULL,
+  `image3` varchar(200) NOT NULL,
+  `description4` varchar(100) NOT NULL,
+  `image4` varchar(200) NOT NULL,
+  `requiredcat1` varchar(50) NOT NULL,
+  `requiredcat2` varchar(50) NOT NULL,
+  `clicks` int(11) NOT NULL,
+  `period` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clicks`
+--
+
+CREATE TABLE `clicks` (
+  `id` int(11) NOT NULL,
+  `adv_id` int(11) NOT NULL,
+  `required` int(11) NOT NULL,
+  `part1` int(11) NOT NULL,
+  `part2` int(11) NOT NULL,
+  `part3` int(11) NOT NULL,
+  `part4` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hostpages`
+--
+
+CREATE TABLE `hostpages` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `url` varchar(150) NOT NULL,
+  `followers` int(11) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `category` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lookups`
+--
+
+CREATE TABLE `lookups` (
+  `id` int(11) NOT NULL,
+  `pagetypes` varchar(25) NOT NULL,
+  `prices` int(11) NOT NULL,
+  `category` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userpages`
+--
+
+CREATE TABLE `userpages` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `followers` int(11) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `generatedurl` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -84,6 +176,36 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `advertise`
+--
+ALTER TABLE `advertise`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clicks`
+--
+ALTER TABLE `clicks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hostpages`
+--
+ALTER TABLE `hostpages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lookups`
+--
+ALTER TABLE `lookups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userpages`
+--
+ALTER TABLE `userpages`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -98,6 +220,30 @@ ALTER TABLE `users`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `advertise`
+--
+ALTER TABLE `advertise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hostpages`
+--
+ALTER TABLE `hostpages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lookups`
+--
+ALTER TABLE `lookups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userpages`
+--
+ALTER TABLE `userpages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`

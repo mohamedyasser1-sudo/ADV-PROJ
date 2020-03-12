82cba7f85649b8b2f19cd87ad0f3adef1c465ee5
Mohamed Yasser <mohamedyasser71230@gmail.com>
2020-03-12 13:02:57 +0200

    datadase fin

--- Database/project.sql
+++ Database/project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2020 at 08:01 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: Mar 11, 2020 at 04:42 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -37,14 +37,6 @@ CREATE TABLE `accepted_adds` (
   `time_to_start` datetime NOT NULL,
   `time_to_end` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `accepted_adds`
---
-
-INSERT INTO `accepted_adds` (`id`, `add_id`, `hosts_id`, `hosts_count`, `required_time`, `time_to_start`, `time_to_end`) VALUES
-(9, 29, '35,46,', 1, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(10, 28, '46,46,', 1, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -100,15 +92,6 @@ CREATE TABLE `advertise` (
   `rejects_id` text NOT NULL DEFAULT ','
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `advertise`
---
-
-INSERT INTO `advertise` (`id`, `post_id`, `pagetype`, `pagename`, `pageurl`, `pagedescription`, `description1`, `image1`, `description2`, `image2`, `description3`, `image3`, `description4`, `image4`, `requiredcat1`, `requiredcat2`, `clicks`, `period`, `price`, `status`, `count_for_A`, `count_for_B`, `count_for_C`, `hosts_id`, `rejects_id`) VALUES
-(27, 37, 'INSTAGRAM', 'good', 'goodday', 'gooogyear', 'it will work', '51603173_1353800621427430_1917240089168052224_o.jpg', '', '', '', '', '', '', 'NEWS&POLITICS', 'NEWS&POLITICS', 500, 1, 50, '1', 0, 0, 0, ',', ','),
-(28, 37, 'FACEBOOK', 'kkk', 'kkkk', 'kkk', 'kkk', '51603173_1353800621427430_1917240089168052224_o.jpg', 'lllllll', '82770308_2426012477663071_4082070345572417536_o.jpg', 'sllllsl', 'Capture.PNG', 'kdkdkdkdk', 'logo.png', 'AUTOS&VECHICLES', 'Sports', 1000, 1, 50, '1', 1, 0, 0, ',46,', ',35,'),
-(29, 37, 'FACEBOOK', 'kkk', 'kkkk', 'kkk', 'kkk', '51603173_1353800621427430_1917240089168052224_o.jpg', 'lllllll', '82770308_2426012477663071_4082070345572417536_o.jpg', 'sllllsl', 'Capture.PNG', 'kdkdkdkdk', 'logo.png', 'AUTOS&VECHICLES', 'Sports', 1000, 2, 50, '1', 0, 1, 0, ',35,46,', ',');
-
 -- --------------------------------------------------------
 
 --
@@ -120,14 +103,7 @@ CREATE TABLE `clicks` (
   `adv_id` int(11) NOT NULL,
   `parts` int(11) NOT NULL DEFAULT 2,
   `hosts` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
-) ;
-
---
--- Dumping data for table `clicks`
---
-
-INSERT INTO `clicks` (`id`, `adv_id`, `parts`, `hosts`, `required`, `part1`, `part2`, `part3`, `part4`, `total`) VALUES
-(13, 51, 1, '{\"22\":2}', NULL, NULL, NULL, NULL, NULL, NULL);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -146,26 +122,6 @@ CREATE TABLE `hostpages` (
   `category` varchar(30) NOT NULL,
   `verification` varchar(10) NOT NULL,
   `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `hostpages`
---
-
-INSERT INTO `hostpages` (`id`, `user_id`, `name`, `url`, `followers`, `followers_cat`, `description`, `category`, `verification`, `status`) VALUES
-(3, 46, 'ehabheab', 'ehabehab', 100, 'A', 'ehabehab', 'SPORTS', 'ehabe', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lookups`
---
-
-CREATE TABLE `lookups` (
-  `id` int(11) NOT NULL,
-  `pagetypes` varchar(25) NOT NULL,
-  `prices` int(11) NOT NULL,
-  `category` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -232,13 +188,6 @@ CREATE TABLE `rejected_adds` (
   `hosts_id` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `rejected_adds`
---
-
-INSERT INTO `rejected_adds` (`id`, `add_id`, `hosts_id`) VALUES
-(10, 28, '46,');
-
 -- --------------------------------------------------------
 
 --
@@ -277,14 +226,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `first_name`, `second_name`, `email`, `password`, `country`, `phone`, `type`, `date`, `code`, `active`) VALUES
-(46, 'ehab', 'ehabehab', 'ehabehab', 'ehabehabehab@ehab.com', 'ehabehab', 'EG', 1121213232, 2, '2020-03-09', 0, 2),
-(47, 'mohamed', 'mohamed', 'mohamed', 'mohamed@mohamed.com', 'ehabehab', 'EG', 1141513524, 1, '2020-03-09', 0, 1);
-
---
 -- Indexes for dumped tables
 --
 
@@ -307,15 +248,15 @@ ALTER TABLE `advertise`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `hostpages`
+-- Indexes for table `clicks`
 --
-ALTER TABLE `hostpages`
+ALTER TABLE `clicks`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `lookups`
+-- Indexes for table `hostpages`
 --
-ALTER TABLE `lookups`
+ALTER TABLE `hostpages`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -350,7 +291,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `accepted_adds`
 --
 ALTER TABLE `accepted_adds`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `admin`
@@ -362,25 +303,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `advertise`
 --
 ALTER TABLE `advertise`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
 
 --
 -- AUTO_INCREMENT for table `clicks`
 --
 ALTER TABLE `clicks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `hostpages`
 --
 ALTER TABLE `hostpages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `lookups`
---
-ALTER TABLE `lookups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `lookups_table`
@@ -392,7 +327,7 @@ ALTER TABLE `lookups_table`
 -- AUTO_INCREMENT for table `rejected_adds`
 --
 ALTER TABLE `rejected_adds`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `userpages`
@@ -404,7 +339,7 @@ ALTER TABLE `userpages`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

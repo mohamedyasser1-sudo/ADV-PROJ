9f84ed83a2a12d9a5da16324d2273624a170bc50
DoaaEssam52 <32943933+DoaaEssam52@users.noreply.github.com>
2020-03-27 22:40:07 +0200

    Add files via upload

---
+++ assets/js/phoneCode.js
@@ -0,0 +1,3 @@
+document.getElementById('country').addEventListener("change", function(){
+    document.getElementById('phone').value = (this.options[this.selectedIndex].getAttribute('data-code'));
+   });

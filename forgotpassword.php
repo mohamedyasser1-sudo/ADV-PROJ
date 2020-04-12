d8b14045557048cdc2126f5d3dcb469a2f309709
EhabElshamy <ehab_elshamy82@yahoo.com>
2020-04-12 09:02:22 +0200

    forget password

--- forgotpassword.php
+++ forgotpassword.php
@@ -1,11 +1,50 @@
 <?php 
 include 'connect/connect.php';
-include 'form_handlers/login.php';
+
+
+if (isset($_POST['send'])) {
+    
+        $selector= bin2hex(random_bytes(8));
+        $token=random_bytes(6);
+        $expires = date("U") + 1800;
+
+  $email = $_POST['email'];
+
+  $query = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'");
+  if(mysqli_num_rows($query) != 1){
+    $showmessage = "This email doesn't look like to be a current user";
+  }else{
+$sql = mysqli_query($conn,"DELETE FROM passreset WHERE resetuser='$email'");
+$hashed_token = password_hash($token,PASSWORD_DEFAULT);
+$insertsql = mysqli_query($conn,"INSERT INTO `passreset`(`resetuser`, `resetselector`, `token`, `expires`) VALUES ('$email','$selector','$token','$expires')");
+$url="http://http://topad.net/test/ADV-PROJ/reset.php?selector= ".$selector."&email = ".$email."&validator=" .bin2hex($token);
+
+ $to = $email;
+ $subject= "Reset Your Password";
+ $message="<p> you have requested to reset your password, you will find a link below, if that was not you please ignore this email; </p>";
+ $message.="<p> here is a link to reset your password :</br>";
+ $message.='<a href="'.$url.'">Reset Your Password</a></p>';
+$headers  = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "Reply-To: TopAD <support@topad.net>"."\r\n";
+$headers .= "From: TopAD  <support@topad.net>"."\r\n";
+    
+if(mail($to,$subject, $message,$headers))
+{
+    //header("Location:singggt.php");
+     echo "Done";
+
+} 
+
+}   
+
+  
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login</title>
+    <title>Reset Password</title>
     <!-- Meta tags -->
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -72,8 +111,8 @@ include 'form_handlers/login.php';
     <div class="w3layouts-two-grids">
         <div class="mid-class">
             <div class="txt-left-side" id="txt-left-side">
-                <h2> Login Here </h2>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget</p>
+                <h2> Forgot Your Password </h2>
+                <p>when you write your address below, you will recieve an email with how to reset your password </p>
                 <form action="" method="post">
                     <div class="form-left-to-w3l">
                         <span class="fa fa-envelope-o" aria-hidden="true"></span>
@@ -81,27 +120,14 @@ include 'form_handlers/login.php';
 
                         <div class="clear"></div>
                     </div>
-                    <div class="form-left-to-w3l ">
 
-                        <span class="fa fa-lock" aria-hidden="true"></span>
-                        <input type="password" name="pass" placeholder="Password" required="">
-                        <div class="clear"></div>
-                    </div>
-                    <div class="main-two-w3ls">
-                        <div class="left-side-forget">
-                            <input type="checkbox" class="checked">
-                            <span class="remenber-me">Remember me </span>
-                        </div>
-                        <div class="right-side-forget">
-                            <a href="forgotpassword.php" class="for">Forgot password...?</a>
-                        </div>
-                    </div>
+
                     <div class="btnn">
-                         <button type="submit" name="login" class="button">Login </button> 
+                         <button type="submit" name="send" class="button">Send Mail </button> 
                       <!--  <input type="submit" name="login" value="Sign Up" placeholder="Sign Up" class="button">-->
                     </div>
                         <?php
-        echo '<div class="text-center p-t-46 p-b-20"><span class="txt2">'.$message.'</span></div>';
+        echo '<div class="text-center p-t-46 p-b-20"><span class="txt2">'.$showmessage.'</span></div>';
     ?>
                 </form>
                 <div class="w3layouts_more-buttn">
